cd36e1380965dc6c6d05dd8f8ff718b941dc4b93
Aji Aulia Fikri <ajiauliafikri@gmail.com>
2024-08-02 13:01:01 +0700

    Project kuliah dengan tim 6 orang

---
+++ contact.php
@@ -0,0 +1,70 @@
+ <div id="contact-page" class="container">
+        <div class="bg">
+            <div class="row">           
+                <div class="col-sm-12">                         
+                    <h2 class="title text-center">Kontak <strong>Kami</strong></h2>                                                      
+                    <div class="bg-light">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.8725448010587!2d107.0014866140447!3d-6.
+      2804819632237185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e698ddf0acc8eab%3A0x90d0ff3cd3b6d825!2sPT.
+      %20Sri%20Dewi%20Utama%20(SDU)!5e0!3m2!1sid!2sid!4v1655213006313!5m2!1sid!2sid"
+       width="1200" height="460" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                    </div>
+                </div>                  
+            </div>      
+            <div class="row">   
+                <div class="col-sm-8">
+                    <div class="contact-form">
+                        <h2 class="title text-center">Saran</h2>
+                        <div class="status alert alert-success" style="display: none"></div>
+                        <form id="main-contact-form" class="contact-form row" name="contact-form" method="post">
+                            <div class="form-group col-md-6">
+                                <input type="text" name="name" class="form-control" required="required" placeholder="Nama">
+                            </div>
+                            <div class="form-group col-md-6">
+                                <input type="email" name="email" class="form-control" required="required" placeholder="Email">
+                            </div>
+                            <div class="form-group col-md-12">
+                                <input type="text" name="subject" class="form-control" required="required" placeholder="Subjek">
+                            </div>
+                            <div class="form-group col-md-12">
+                                <textarea name="message" id="message" required="required" class="form-control" rows="8" placeholder="Pesan anda!"></textarea>
+                            </div>                        
+                            <div class="form-group col-md-12">
+                                <input type="submit" name="submit" class="btn btn-primary pull-right" value="Submit">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="contact-info">
+                        <h2 class="title text-center">Kontak Info</h2>
+                        <address>
+                            <p>Pt. Sri Dewi Utama</p>
+                            <p>Perumahan Taman Narogong Indah</p>
+                            <p>Jl. Bojong Indah 13 Blok D7 No. 7, RT 08/RW 11</p>
+                            <p>Bekasi, Indonesia - 17116</p>
+                            <p>FAX. : 021-8243-7106</p>
+                            <p>Email : pt.sdutama@gmail.com</p>
+                        </address>
+                        <div class="social-networks">
+                            <h2 class="title text-center">Sosial Media</h2>
+                            <ul>
+                                <li>
+                                    <a href="#"><i class="fa fa-facebook"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-twitter"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-google-plus"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-youtube"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>              
+            </div>  
+        </div>  
+    </div><!--/#contact-page-->
---
+++ home.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" type="text/css" href="style.css">
+
+
+    <title>PT. Sri Dewi Utama</title>
+  </head>
+  <body>
+    
+    
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4  text-center"><strong>MOVER AND SPARE PART ATM</strong></h1>
+        </div>
+      </div>
+
+
+        <h3 class="mt-5 text-center">TENTANG KAMI</h3>
+        <hr>
+
+      <div class="bg-light">
+        <div class="container text-justify p-4">
+                PT. Sri Dewi Utama terbentuk dari sejumlah teknisi terampil dan berpengalaman di dalam bidang ATM (Auto Teller Machine),
+                khususnya dalam hal pemindahan/pengantaran mesin dan pemasangan askesori pengamannya. 
+                Berbagai jenis dan merek mesin ATM, antara lain Diebold, Wincor, 
+                NCR atau hyosung bisa ditangani dengan baik oleh PT. Sri Dewi Utama.
+                Kegiatan Bisnis PT. Sri Dewi Utama di bidang ini berawal dari tahun 2010. 
+                PT. Sri Dewi Utama adalah perusahaan pemenang tender pekerjaan pemasangan 
+                angkur pengaman mesin ATM beberapa bank ternama di Indonesia.
+                Pada tahun 2013 PT. Sri Dewi Utama berhasil melengkapi semua persyaratan legalitasnya 
+                sebagai badan hukum dan resmi menjadi perseroan terbatas (PT).
+
+        </div>
+    </div>
+
+    <div class="container">
+      <div class="row text-center">
+          <div class="col-md-6">
+              <h3>VISI</h3>
+          <div class="text-center">
+            Menjadi perusahaan yang unggul dalam pengelolaah dan pengembangan teknologi informasi.
+          </div>
+            </div>
+
+          <div class="col-md-6">
+              <h3>MISI</h3>
+          <div class="text-center">
+              Menjalankan usaha di bidang teknologi, terutama dalam hal pengadaan barang dan jasa pengriman.
+          </div> 
+            </div>
+          
+      </div>
+  </div>
+
+
+    <h3 class="mt-5 text-center">PRODUK KAMI</h3>
+    <hr>
+
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-md-6 p-3">
+                <h2><i class="fa-solid fa-truck-pickup"></i></h2>
+                <h3>MOVER</h3>
+
+            </div>
+
+            <div class="col-md-6 p-3">
+                <h2><i class="fa-solid fa-hard-drive"></i></h2>
+                <h3>SPARE PART ATM</h3>
+                 
+            </div>
+            
+        </div>
+    </div>
+
+    <!--BAGIAN INI ADALAH BAGIAN CLIENT-->
+    <h3 class="mt-5 text-center">BEKERJA SAMA DENGAN</h3>
+    <hr>
+
+    <div class="bg-light">
+        <div class="row text-center">
+          <div class="col-md-4">
+            <a href="">
+            <img src="LOGO/bri.png" width="300px" height="200px">
+            </a>
+          </div>
+          <div class="col-md-4">
+            <a href="">
+            <img src="LOGO/nisp.png" width="400px" height="250px">
+            </a>
+          </div>
+          <div class="col-md-4">
+            <a href="">
+            <img src="LOGO/bni.png" width="300px" height="200px">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
